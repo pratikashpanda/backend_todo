58b9c83f1f3f2eeeb444a85a0cfaf31274a56a77
pratikash <pratikashpanda.1234@gmail.com>
2023-06-09 18:44:19 +0530

    error handled

--- controllers/task.js
+++ controllers/task.js
@@ -1,63 +1,71 @@
 import { Task } from "../models/task.js";
 
 export const newTask = async (req, res, next) => {
-  const { title, description } = req.body;
-  await Task.create({
-    title,
-    description,
-    user: req.user,
-  });
+  try {
+    const { title, description } = req.body;
+    await Task.create({
+      title,
+      description,
+      user: req.user,
+    });
 
-  res.status(201).json({
-    success: true,
-    message: "Task Added Successfully",
-  });
+    res.status(201).json({
+      success: true,
+      message: "Task Added Successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getMyTasks = async (req, res, next) => {
-  const userid = req.user._id;
+  try {
+    const userid = req.user._id;
 
-  const tasks = await Task.find({ user: userid });
+    const tasks = await Task.find({ user: userid });
 
-  res.status(200).json({
-    success: true,
-    tasks,
-  });
+    res.status(200).json({
+      success: true,
+      tasks,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateTasks = async (req, res, next) => {
-  const { id } = req.params;
-  const task = await Task.findById(id);
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
 
-   if (!task)
-     return res.status(404).json({
-       success: False,
-       message: "invalid id",
-     });
+    if (!task) return next(new Error("Invalid ID"));
 
-  task.isCompleted = !task.isCompleted;
-  await task.save();
+    task.isCompleted = !task.isCompleted;
+    await task.save();
 
-  res.status(200).json({
-    success: true,
-    messsage: "task updated",
-  });
+    res.status(200).json({
+      success: true,
+      messsage: "task updated",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteTasks = async (req, res, next) => {
-  const { id } = req.params;
-  const task = await Task.findById(id);
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
 
-  if (!task)
-    return res.status(404).json({
-      success: False,
-      message: "invalid id",
+    if (!task) return next(new Error("Invalid ID"));
+
+    task.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      messsage: "task deleted",
     });
-
-  task.deleteOne();
-
-  res.status(200).json({
-    success: true,
-    messsage: "task deleted",
-  });
+  } catch (error) {
+    next(error);
+  }
 };
--- controllers/user.js
+++ controllers/user.js
@@ -6,41 +6,35 @@ import jwt from "jsonwebtoken";
 export const getAllUsers = async (req, res) => {};
 
 export const login = async (req, res, next) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email }).select("+password");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
 
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "Invalid Email or Password",
-    });
+    if (!user) return next(new Error("Invalid Email or Password"));
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return next(new Error("Invalid Email or Password"));
+
+    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  } catch (error) {
+    next(error);
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(404).json({
-      success: false,
-      message: "Invalid Email or Password",
-    });
-  }
-
-  sendCookie(user, res, `Welcome back, ${user.name}`, 200);
 };
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = await User.findOne({ email });
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
 
-  if (user)
-    return res.status(404).json({
-      success: false,
-      message: "User already exist",
-    });
+    if (user) return next(new Error("User already exist"));
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  user = await User.create({ name, email, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({ name, email, password: hashedPassword });
 
-  sendCookie(user, res, "Registered Successfully", 201);
+    sendCookie(user, res, "Registered Successfully", 201);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getMyDetails = (req, res) => {
@@ -53,7 +47,11 @@ export const getMyDetails = (req, res) => {
 export const logout = (req, res) => {
   res
     .status(200)
-    .cookie("token", "", { expires: new Date(Date.now()) })
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
+    })
     .json({
       success: true,
       user: req.user,
